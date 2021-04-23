4449718a93774aba27679d8a30ebdcf5186030f7
Anton Kozlov <anton.vladimirovich87@gmail.com>
2021-04-23 17:08:49 +0300

    Added header and main container

---
+++ vue/src/components/Content.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="main">
+        <Posts/>
+    </div>
+</template>
+
+<script>
+    import Posts from './Posts'
+
+    export default {
+        name: 'Content',
+        components: {
+            Posts
+        }
+    }
+</script>
+
+<style scoped>
+    .main {
+        border: 1px solid #ccc;
+        padding: 15px;
+        margin-top: 10px;
+    }
+</style>
---
+++ vue/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>Header of {{ msg }}</header>
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+        props: {
+            msg: String
+        }
+    }
+</script>
+
+<style scoped>
+    header {
+        padding: 15px;
+        background: #accba6;
+        font-weight: bold;
+    }
+</style>
